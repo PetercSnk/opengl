e7cd59a83edca1c2cfb8bf890a340be4745a32f6
Peter Snook <snookp2@cardiff.ac.uk>
2024-11-27 13:40:14 +0000

    check create program func

--- project/project/src/shader.cpp
+++ project/project/src/shader.cpp
@@ -27,12 +27,12 @@ std::string read_binary_file(const char* path)
 	}
 }
 
-unsigned int compile_shader(GLenum type, const char* shader)
+unsigned int compile_shader(GLenum type, const char* source)
 {
 	// create shader object for the specified type
 	unsigned int id = glCreateShader(type);
 	// attach the shader source to the shader object
-	glShaderSource(id, 1, &shader, nullptr);
+	glShaderSource(id, 1, &source, nullptr);
 	// compile shader
 	glCompileShader(id);
 	// check for compilation errors
@@ -46,9 +46,41 @@ unsigned int compile_shader(GLenum type, const char* shader)
 		glGetShaderInfoLog(id, 512, NULL, log);
 		std::cout << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << "compilation failed" << std::endl;
 		std::cout << log << std::endl;
-		// free occupied memory, effectively undoing the effects of the call to glCreateShader
+		// free occupied memory
 		glDeleteShader(id);
 		return 0;
 	}
 	return id;
+}
+
+unsigned int create_shader(const char* vertex_shader_source, const char* fragment_shader_source)
+{
+	// compile both shaders
+	unsigned int vertex_shader_id = compile_shader(GL_VERTEX_SHADER, vertex_shader_source);
+	unsigned int fragment_shader_id = compile_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
+	// create program object
+	unsigned int program_id = glCreateProgram();
+	// attach and link compiled shaders to program object
+	glAttachShader(program_id, vertex_shader_id);
+	glAttachShader(program_id, fragment_shader_id);
+	glLinkProgram(program_id);
+	// check for linking errors
+	int success;
+	char log[512];
+	// retrieve the linking status parameter from the program object
+	glGetProgramiv(program_id, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		// retrieve the information log for the program object
+		glGetProgramInfoLog(program_id, 512, NULL, log);
+		std::cout << "Linking failed" << std::endl;
+		std::cout << log << std::endl;
+		// free occupied memory
+		glDeleteProgram(program_id);
+		return 0;
+	}
+	// delete shader objects after linking
+	glDeleteShader(vertex_shader_id);
+	glDeleteShader(fragment_shader_id);
+	return program_id;
 }
