ade5b94b8be604960ed26ad50633e5de88361fe6
petercsnk <peter.ch.snook@gmail.com>
2025-03-20 16:20:14 +0000

    updated some comments

--- project/project/src/shader.cpp
+++ project/project/src/shader.cpp
@@ -2,22 +2,22 @@
 
 std::string read_binary_file(const char* path)
 {
-	// open binary file with current get position pointer set to eof
+	// opens binary file with current get position pointer set to eof
 	std::ifstream file(path, std::ios::binary | std::ios::ate | std::ios::in);
-	// check if opened successfully, else print error
+	// checks if opened successfully, else print error
 	if (file.is_open())
 	{
 		// placeholder for file content
 		std::string content;
-		// find size of file
+		// finds size of file
 		int size = file.tellg();
-		// resize content 
+		// resizes content 
 		content.resize(size);
-		// set current get position pointer to beginning of file
+		// sets current get position pointer to beginning of file
 		file.seekg(0, std::ios::beg);
-		// read file as block
+		// reads file as block
 		file.read(&content[0], size);
-		// close file and return content
+		// closes file and returns content
 		file.close();
 		return content;
 	}
@@ -29,24 +29,24 @@ std::string read_binary_file(const char* path)
 
 GLuint compile_shader(GLenum type, const GLchar* source)
 {
-	// create shader object for the specified type
+	// creates shader object for the specified type
 	GLuint shader = glCreateShader(type);
-	// attach the shader source to the shader object
+	// attaches the shader source to the shader object
 	glShaderSource(shader, 1, &source, nullptr);
-	// compile shader
+	// compiles shader
 	glCompileShader(shader);
-	// check for compilation errors
+	// checks for compilation errors
 	GLint success;
 	GLchar log[512];
-	// retrieve the compilation status parameter from the shader object
+	// retrieves the compilation status parameter from the shader object
 	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
 	if (!success)
 	{
-		// retrieve the information log for the shader object
+		// retrieves the information log for the shader object
 		glGetShaderInfoLog(shader, 512, NULL, log);
 		std::cout << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << " compilation failed" << std::endl;
 		std::cout << log << std::endl;
-		// free occupied memory
+		// frees occupied memory
 		glDeleteShader(shader);
 		return 0;
 	}
@@ -55,27 +55,27 @@ GLuint compile_shader(GLenum type, const GLchar* source)
 
 GLuint create_shader(const GLchar* vertex_source, const GLchar* fragment_source)
 {
-	// compile vertex and fragment shader
+	// compiles vertex and fragment shader
 	GLuint vertex_shader = compile_shader(GL_VERTEX_SHADER, vertex_source);
 	GLuint fragment_shader = compile_shader(GL_FRAGMENT_SHADER, fragment_source);
-	// create program object
+	// creates program object
 	GLuint program = glCreateProgram();
-	// attach and link compiled shaders to program object
+	// attaches and links compiled shaders to program object
 	glAttachShader(program, vertex_shader);
 	glAttachShader(program, fragment_shader);
 	glLinkProgram(program);
-	// check for linking errors
+	// checks for linking errors
 	GLint success;
 	GLchar log[512];
-	// retrieve the linking status parameter from the program object
+	// retrieves the linking status parameter from the program object
 	glGetProgramiv(program, GL_LINK_STATUS, &success);
 	if (!success)
 	{
-		// retrieve the information log for the program object
+		// retrieves the information log for the program object
 		glGetProgramInfoLog(program, 512, NULL, log);
 		std::cout << "Linking failed" << std::endl;
 		std::cout << log << std::endl;
-		// free occupied memory
+		// frees occupied memory
 		glDeleteProgram(program);
 		return 0;
 	}
@@ -87,12 +87,12 @@ GLuint create_shader(const GLchar* vertex_source, const GLchar* fragment_source)
 
 Shader::Shader(const char* vertex_path, const char* fragment_path)
 {
-	// read in vertex and fragment shader
+	// reads in vertex and fragment shaders
 	std::string vertex_source_str = read_binary_file(vertex_path);
 	std::string fragment_source_str = read_binary_file(fragment_path);
 	const GLchar* vertex_source = vertex_source_str.c_str();
 	const GLchar* fragment_source = fragment_source_str.c_str();
-	// create program object with given shaders
+	// creates program object with given shaders
 	program = create_shader(vertex_source, fragment_source);
 }
 
