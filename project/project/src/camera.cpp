10eb03dddf2902fd4240135d24aff3275f0463b0
Peter Snook <snookp2@cardiff.ac.uk>
2025-04-01 12:00:47 +0100

    added camera control

--- project/project/src/camera.cpp
+++ project/project/src/camera.cpp
@@ -1,13 +1,15 @@
 #include "camera.h"
 
-Camera::Camera(glm::vec3 init_position = POSITION, glm::vec3 init_world_up = WORLD_UP, float init_yaw = YAW, float init_pitch = PITCH, float init_sens = SENS, float init_fov = FOV)
+Camera::Camera(glm::vec3 position = POSITION, glm::vec3 front = FRONT, glm::vec3 world_up = WORLD_UP, float yaw = YAW, float pitch = PITCH, float sens = SENS, float speed = SPEED, float fov = FOV)
 {
-	position = init_position;
-	world_up = init_world_up;
-	yaw = init_yaw;
-	pitch = init_pitch;
-	sens = init_sens;
-	fov = init_fov;
+	this->position = position;
+	this->front = front;
+	this->world_up = world_up;
+	this->yaw = yaw;
+	this->pitch = pitch;
+	this->sens = sens;
+	this->speed = speed;
+	this->fov = fov;
 	update();
 }
 
@@ -18,11 +20,24 @@ glm::mat4 Camera::get_view_matrix()
 
 void Camera::update()
 {
-	glm::vec3 new_front;
-	new_front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	new_front.y = sin(glm::radians(pitch));
-	new_front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-	front = glm::normalize(new_front);
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front = glm::normalize(front);
 	right = glm::normalize(glm::cross(front, world_up));
 	up = glm::normalize(glm::cross(right, front));
+}
+
+void Camera::process_keyboard(directions direction, float delta_time)
+{
+	float velocity = speed * delta_time;
+	if (direction == FORWARD)
+		position += speed * front;
+	if (direction == BACKWARD)
+		position -= speed * front;
+	if (direction == LEFT)
+		position -= glm::normalize(glm::cross(front, up)) * speed;
+	if (direction == RIGHT)
+		position += glm::normalize(glm::cross(front, up)) * speed;
 }
--- project/project/src/camera.h
+++ project/project/src/camera.h
@@ -11,10 +11,12 @@ enum directions {
 };
 
 glm::vec3 POSITION = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 FRONT = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 WORLD_UP = glm::vec3(0.0f, 1.0f, 0.0f);
 float YAW = -90.0f;
 float PITCH = 0.0f;
 float SENS = 0.1f;
+float SPEED = 2.5f;
 float FOV = 45.0f;
 
 class Camera
@@ -29,11 +31,12 @@ public:
 	// euler angles
 	float yaw;
 	float pitch;
-	// camera options
+	// cameras options
 	float sens;
+	float speed;
 	float fov;
 	//
-	Camera(glm::vec3 position, glm::vec3 world_up, float yaw, float pitch, float sens, float fov);
+	Camera(glm::vec3 front, glm::vec3 direction, glm::vec3 world_up, float yaw, float pitch, float sens, float speed, float fov);
 	glm::mat4 get_view_matrix();
 	void process_keyboard(directions direction, float delta_time);
 	void process_mouse(float dx, float dy);
