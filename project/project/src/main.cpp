b3276c96dc02147a1a5bcf607f06b70125ce56cc
petercsnk <peter.ch.snook@gmail.com>
2025-04-02 15:42:15 +0100

    working light shader

--- project/project/src/main.cpp
+++ project/project/src/main.cpp
@@ -63,85 +63,92 @@ int main()
 	//const char* f = "C:\\Users\\c1842512\\OneDrive - Cardiff University\\Repo\\opengl\\project\\project\\src\\shaders\\fragment.frag";
 	const char* v = "D:\\Repository\\opengl\\project\\project\\src\\shaders\\vertex.vert";
 	const char* f = "D:\\Repository\\opengl\\project\\project\\src\\shaders\\fragment.frag";
+	const char* l = "D:\\Repository\\opengl\\project\\project\\src\\shaders\\light.frag";
 	Shader shader(v, f);
+	Shader light(v, l);
 
-	float vertices[] = {
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	float cube[] = {
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
 
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
 
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
 
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
 
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f, -0.5f,
+		 0.5f, -0.5f,  0.5f,
+		 0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f,  0.5f,
+		-0.5f, -0.5f, -0.5f,
 
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+		-0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f, -0.5f,
+		 0.5f,  0.5f,  0.5f,
+		 0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f,  0.5f,
+		-0.5f,  0.5f, -0.5f
 	};
+
+	glm::vec3 light_position(0.0f, 0.0f, 0.0f);
+
+	glm::vec3 cube_positions[] = {
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f),
+		glm::vec3(-10.3f,  1.0f, -1.5f)
+	};
+
+	VBO vbo(sizeof(cube), cube);
 
 	VAO vao;
 	vao.bind();
-	VBO vbo(sizeof(vertices), vertices);
-	vao.link_attrib(vbo, 0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	vao.link_attrib(vbo, 1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	vao.link_attrib(vbo, 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	vao.unbind();
-	
 
+	VAO light_vao;
+	light_vao.bind();
+	light_vao.link_attrib(vbo, 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	light_vao.unbind();
+	
 	//const char* container = "C:\\Users\\c1842512\\OneDrive - Cardiff University\\Repo\\opengl\\project\\project\\src\\textures\\container.jpg";
 	//const char* awesomeface = "C:\\Users\\c1842512\\OneDrive - Cardiff University\\Repo\\opengl\\project\\project\\src\\textures\\awesomeface.png";
-	const char* container = "D:\\Repository\\opengl\\project\\project\\src\\textures\\container.jpg";
-	const char* awesomeface = "D:\\Repository\\opengl\\project\\project\\src\\textures\\awesomeface.png";
-	Texture tex1(container, GL_RGB, GL_REPEAT, GL_LINEAR);
-	Texture tex2(awesomeface, GL_RGBA, GL_REPEAT, GL_LINEAR);
-
-	shader.bind();
-	shader.set_int("texture1", 0);
-	shader.set_int("texture2", 1);
+	//const char* container = "D:\\Repository\\opengl\\project\\project\\src\\textures\\container.jpg";
+	//const char* awesomeface = "D:\\Repository\\opengl\\project\\project\\src\\textures\\awesomeface.png";
+	//Texture tex1(container, GL_RGB, GL_REPEAT, GL_LINEAR);
+	//Texture tex2(awesomeface, GL_RGBA, GL_REPEAT, GL_LINEAR);
+	//shader.bind();
+	//shader.set_int("texture1", 0);
+	//shader.set_int("texture2", 1);
 
 	glEnable(GL_DEPTH_TEST);
-
-	glm::vec3 cubePositions[] = {
-	glm::vec3(0.0f,  0.0f,  0.0f),
-	glm::vec3(2.0f,  5.0f, -15.0f),
-	glm::vec3(-1.5f, -2.2f, -2.5f),
-	glm::vec3(-3.8f, -2.0f, -12.3f),
-	glm::vec3(2.4f, -0.4f, -3.5f),
-	glm::vec3(-1.7f,  3.0f, -7.5f),
-	glm::vec3(1.3f, -2.0f, -2.5f),
-	glm::vec3(1.5f,  2.0f, -2.5f),
-	glm::vec3(1.5f,  0.2f, -1.5f),
-	glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
@@ -155,23 +162,23 @@ int main()
 		delta_time = current_frame - last_frame;
 		last_frame = current_frame;
 
-		tex1.bind(GL_TEXTURE0);
-		tex2.bind(GL_TEXTURE1);
-
-		shader.set_float("m", m);
+		//tex1.bind(GL_TEXTURE0);
+		//tex2.bind(GL_TEXTURE1);
 
 		shader.bind();
-		vao.bind();
+		shader.set_vec3("objectColor", glm::vec3(1.0f, 0.5f, 0.31f));
+		shader.set_vec3("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
 
 		glm::mat4 projection = glm::perspective(glm::radians(camera.fov), (float)width / (float)height, 0.1f, 100.f);
-		shader.set_mat4fv("projection", projection);
 		glm::mat4 view = camera.get_view_matrix();
-		shader.set_mat4fv("view", view);
+		shader.set_mat4("projection", projection);
+		shader.set_mat4("view", view);
+		glm::mat4 model = glm::mat4(1.0f);
 
 		for (unsigned int i = 0; i < 10; i++)
 		{
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, cube_positions[i]);
 			float angle = 20.0f * (i + 1);
 			if (i == 0 || i % 3 == 0) {
 				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.5f, 0.0f));
@@ -180,9 +187,19 @@ int main()
 			{
 				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.5f, 0.0f));
 			}
-			shader.set_mat4fv("model", model);
+			shader.set_mat4("model", model);
+			vao.bind();
 			glDrawArrays(GL_TRIANGLES, 0, 36);
 		}
+
+		light.bind();
+		light.set_mat4("projection", projection);
+		light.set_mat4("view", view);
+		model = glm::mat4(1.0f);
+		model = glm::scale(model, glm::vec3(0.2f));
+		light.set_mat4("model", model);
+		light_vao.bind();
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -225,20 +242,6 @@ void process_input(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-	{
-		if (m >= 1.0f)
-			m = 1.0f;
-		else
-			m += 0.0001f;
-	}
-	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-	{
-		if (m <= 0.0f)
-			m = 0.0f;
-		else
-			m -= 0.0001f;
-	}
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 		camera.process_keyboard(FORWARD, delta_time);
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
